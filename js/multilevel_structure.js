274f05695b56ffa3db177d1a7a33d215bfc10c1a
Olga Zhilkova <olga.progr@gmail.com>
2019-05-17 13:10:31 +0300

    Changing into more professional function jQuery module, but not working version yet

--- js/multilevel_structure.js
+++ js/multilevel_structure.js
@@ -5,114 +5,180 @@
  * Version: 1.0 
  */
 //jQuery.noConflict();
-(function($){
+;(function ( $, window, document, undefined ) {
+	
+	let pluginName = "multilevelStructure",
+	  defaults = {
+			//DOM element which is the action type for every WBS row
+	        button: "button",
+	        //a class name to show what action now the row is under 
+	        activeClass: "active",
+	        //a class name to show which row now is on focus
+	        //activeRow: "table-active",
+	        onSave: function(elem){ return true},
+	        fields: ["title","cost","period"], 
+		};
+	
+    /* necessary html tags for working plugin properly */
+    const structure = {
+      containerTable: "tbody",
+      containerEntry: "tr",
+      valSelector: "input",
+    };
     
-    $.fn.multilevelStructure = function(options)
+    const readOnlyAttr="readonly";
+    const buttonNew="new";
+    const buttonModify="modify";
+    const buttonDiscard='discard';
+    const buttonSave='save';
+    const buttonMoveUp='moveUp';
+    const buttonMoveDown='doveDown';
+    const buttonEncreaseLevel='encreaseLevel';
+    const buttonDecreaseLevel='decreaseLevel';
+    const buttonsList=["edit","new","delete","save","moveUp","moveDown","levelDown","levelUp"];
+    const levelSeparator= "_";
+    const dot = ".";
+    const idAttribute =  "id";    
+    
+    
+	
+	function Plugin( element, options ) {
+		
+		this.element = element;
+		this.settings = $.extend( true, {}, defaults, options);
+		this._defauls = defaults;
+		this._name = pluginName;
+		 
+        this.init();
+	}
+	
+	
+	Plugin.prototype.init = function ()
+	{
+		
+		let settings = (this.settings);
+		$(this.element).each(function()
+		{
+		    let current_row=$(this).closest(structure.containerEntry),
+	         classRange=$(this).attr("class"),
+	       	 actionClass;
+	       	
+	       	for (let i = 0; i < buttonsList.length; i++)
+	       	{
+	       		actionClass = new RegExp('(^| )' + buttonsList[i] + '( |$)', 'gi');
+	       		if (actionClass.test(classRange))
+	       		{
+	       			  //removing active class from other buttons
+	       			  $(current_row).find(settings.button).removeClass(settings.activeClass);	
+	       			  
+	                  //making button active with needed CSS class
+	                  $(this).addClass(settings.activeClass);
+	                  switch(buttonsList[i]) {
+	                    case buttonsList[0]:
+	                      //stateEdit(current_row);
+	                      break;
+	                    case buttonsList[1]:
+	                      stateNew(current_row,settings);
+	                      break;
+	                    case buttonsList[3]:
+	                      //stateSave(current_row);
+	                      break;
+	                    case buttonsList[6]:
+	                      //stateLevelDown(current_row);
+	                      break;
+	                   default:
+	                      return false;
+	               }
+	                  
+	                  
+	       		}
+	        }
+		});
+	}
+	
+	
+    //new row is added into the table
+    const stateNew = function(current_row,settings)
     {
-      var defaults = {
-        element: "button",
-        editClass: "edit",
-        activeButtonClass: "active",
-        activeRow: "table-active",
-        prefixRowId: "deliv",
-        idAttribute: "id",
-        levelSeparator: "_",
-        onSave: function(elem){ return true},
-        fields: ["title","cost","period"], 
-      };
-      
-      const constant= {
-        buttons: ["edit","new","delete","save","moveUp","moveDown","levelDown","levelUp"],
-        containerTable: "tbody",
-        containerEntry: "tr",
-        valSelector: "input",
-        dot: ".",
-        readAttr: "readonly"
-      };
-      
-      /* future possible options */
-      //the choice to edit just one field
-      
-      
-      /* necessary html tags for working plugin properly */
-      //all classes in Constant object must exist in html
-      //check additional classes that uses user or bootstrap styles and safe them in html
-      
-      //merging user setting and defining standard settings
-      var settings = $.extend( true, {}, defaults, options);
-      
-      var Implement = function(action,current_row){
-        
-        switch(action) {
-            case constant.buttons[0]:
-                stateEdit(current_row);
-                break;
-            case constant.buttons[1]:
-                stateNew(current_row);
-                break;
-            case constant.buttons[3]:
-                stateSave(current_row);
-                break;
-            case constant.buttons[6]:
-                stateLevelDown(current_row);
-                break;
-            default:
-                return false;
-        }
-
-      };
-      
-        //edit start button action to make any field edible
-        var stateEdit = function(current_row)
-        {
-          //set current row active
-          $(current_row).parent().children().removeClass(settings.activeRow);
-          $(current_row).addClass(settings.activeRow);
-           //there must the choice of editing whole table row
-           $.each(settings.fields, function( index, value ) {
-            $(current_row).find(constant.valSelector+constant.dot+value).removeAttr(constant.readAttr);
-          });
+      //changing the styles of the row and make the input fields read only
+      let doSave=settings.onSave(current_row);
+      if(doSave===true)
+      {
+          //clonning the current row on lower position with the same level
+          let rowNew = $(current_row).clone(true).insertAfter(current_row);
           
-           //focus to only first element
-          $(current_row).find(constant.valSelector).first().focus();
-          //another button for saving the value must be changed to active state
-        
-        };
+          rowNew.attr(idAttribute,generateLevel($(current_row).attr(idAttribute)));
+          
+          //all values of the new row must be empty
+          for (i = 0, len = settings.fields.length; i < len; i++) {
+              rowNew.find(valSelector+'.'+settings.fields[i]).val('');
+          }
+          stateEdit(rowNew);
+          
+          let nextRow=$(rowNew).next();
+         
+          while($(nextRow).index()>0)
+          {
+            $(nextRow).attr(idAttribute,generateLevel($(nextRow).attr(idAttribute)));
+            nextRow=$(nextRow).next();
+          }
+      }
+
+    }
+	
+	  
+     
+     /*
+      * edit button action to make any field edible
+      */
+     const stateEdit = function(current_row)
+     {
+       //set current row active
+       $(current_row).parent().children().removeClass(settings.activeRow);
+       $(current_row).addClass(settings.activeRow);
+        //there must the choice of editing whole table row
+        $.each(settings.fields, function( index, value ) {
+         $(current_row).find(constant.valSelector+constant.dot+value).removeAttr(readOnlyAttr);
+       });
+       
+        //focus to only first element
+       $(current_row).find(constant.valSelector).first().focus();
+       //another button for saving the value must be changed to active state
+     
+     };
+     
+	
+	
+	 // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if ( !$.data(this, "plugin_" + pluginName )) {
+                $.data( this, "plugin_" + pluginName,
+                new Plugin( this, options ));
+            }
+        });
+    }
+      
+      
+ 
+     
+
+      
+     
+      
+      
+
+      
+   
             
      
       
       
-      //new row is added into the table
-      var stateNew = function(current_row)
-      {
-        //changing the styles of the row and make the input fields read only
-        var doSave=settings.onSave(current_row);
-        if(doSave===true)
-        {
-            //clonning the current row on lower position with the same level
-            var rowNew = $(current_row).clone(true).insertAfter(current_row);
-            
-            rowNew.attr(settings.idAttribute,generateLevel($(current_row).attr(settings.idAttribute)));
-            
-            //all values of the new row must be empty
-            for (i = 0, len = settings.fields.length; i < len; i++) {
-                rowNew.find(constant.valSelector+'.'+settings.fields[i]).val('');
-            }
-            stateEdit(rowNew);
-            
-            var nextRow=$(rowNew).next();
-           
-            while($(nextRow).index()>0)
-            {
-              $(nextRow).attr(settings.idAttribute,generateLevel($(nextRow).attr(settings.idAttribute)));
-              nextRow=$(nextRow).next();
-            }
-        }
 
-      }
       
       //current row is saved with ajax request
-      var stateSave = function(current_row)
+      const stateSave = function(current_row)
       {
         $.each(settings.fields, function( index, value )
         {
@@ -125,16 +191,16 @@
       /*
       * Changing level of the entry to make as a subdirectory of the parent entry
       */
-      var stateLevelDown = function(current_row)
+      const stateLevelDown = function(current_row)
       {
         //generating new ID of the level
-        var nextLevel = $(current_row).attr(settings.idAttribute)+settings.levelSeparator;
+        var nextLevel = $(current_row).attr(constant.idAttribute)+constant.levelSeparator;
         
         //check the next row for level
-        var nextRowID=$(current_row).next().attr(settings.idAttribute);
-        var levelIdPatt = new RegExp($(current_row).attr(settings.idAttribute)+settings.levelSeparator+"\\d");
+        var nextRowID=$(current_row).next().attr(constant.idAttribute);
+        var levelIdPatt = new RegExp($(current_row).attr(constant.idAttribute)+constant.levelSeparator+"\\d");
         //check if the current row has parennt to move it ti  lower level
-        var checkLevelCurrent=findlevel($(current_row).attr(settings.idAttribute));
+        var checkLevelCurrent=findlevel($(current_row).attr(constant.idAttribute));
         if(checkLevelCurrent>1)
         {
           //reordering all ID of children rows and their children
@@ -143,8 +209,8 @@
             function(index)
             {
               //slice ID to find the prefic before numbers of the level 
-              var id= $(this).attr(settings.idAttribute);       
-              return levelIdPatt.test($(this).attr(settings.idAttribute));
+              var id= $(this).attr(constant.idAttribute);       
+              return levelIdPatt.test($(this).attr(constant.idAttribute));
             }).length;
         }
           
@@ -154,7 +220,7 @@
       /*
       * Generate the next level relating to the current level
       */
-      var generateLevel = function(currentId)
+      const generateLevel = function(currentId)
       {
         var newOrder, newId; 
         //the last number of the new row must be increased to 1
@@ -176,10 +242,10 @@
       /*
       * Get the number of the most last sublevel of ID
       */
-      var findNumberLevel = function(currentId)
+      const findNumberLevel = function(currentId)
       {
         var parsedLevel=0, 
-            findLast = currentId.lastIndexOf(settings.levelSeparator);
+            findLast = currentId.lastIndexOf(constant.levelSeparator);
         
         //to check all number for separator symbols must be determined to exclude them from digitals
         if(findLast>-1)
@@ -197,7 +263,7 @@
       /*
       * Get the level of the current row
       */
-      var findLevel = function(currentId)
+      const findLevel = function(currentId)
       {
         var i=0, levelSep=findNumberLevel(currentId);
         
@@ -212,69 +278,9 @@
       }
       
       
-      /*
-      * Deleting row 
-      */
-      var stateDelete = function(current_row)
-      {
-        //active state for row
-        //asking before deleting the row if the user sure about it
-        //check is the winodw is plugin or local window
-        //deleting the row from the DOM
-        //deleting row from the DB
-      }
       
-      
-      /*
-      * reordering all table with new row and new row ID
-      */
-      var reorderTable = function(current_row)
-      {
-        //the next rows must be rearanged with last number increased on one more count
-        //all rows that are childrens from the first level rows and further levels must have first and other needed numbers incresed
-      }
-      
-      
-      /*
-      * main implementation for assigning elements
-      */
-      var bondButtons = function()
-      {
-        var button=$(this),
-          current_row=$(this).closest(constant.containerEntry);
-        
-        //identifing objects for every action
-        $.each(constant.buttons, function( index, value ) {
-            
-            if ($(button).hasClass(value)===true)
-            {
-              //removing active class from other buttons
-              $(current_row).find(settings.element).removeClass(settings.activeButtonClass);
-              //making button active with needed CSS class
-              $(button).addClass(settings.activeButtonClass);
-              Implement(value,current_row); 
-            }
-            
-          });
-      }
-       
-      return this.bind('click',bondButtons);
-    };
- 
 
-    // Private function for debugging.
-    function debug_props(obj)
-    {
-      window.console.log("Attribute of the button: " + obj.attr('class'));
-        
-    };
-    
-    //
-    function find_length(obj)
-    {
-      window.console.log("Field: " + obj.length);
        
-    };
- 
+      
 
 })(jQuery);
