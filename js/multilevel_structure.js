0dde970c88df1a8d77016b265fa8a37c56e284dd
Olga Zhilkova <olga.progr@gmail.com>
2019-06-18 11:08:00 +0300

    Template for separate table row with addition of necessary buttons

--- js/multilevel_structure.js
+++ js/multilevel_structure.js
@@ -4,20 +4,39 @@
  * Author: Olga Zhilkova
  * Version: 1.0 
  */
-//jQuery.noConflict();
 ;(function ( $, window, document, undefined ) {
 	
-	let pluginName = "multilevelStructure",
-	  defaults = {
-			//DOM element which is the action type for every WBS row
-	        button: "button",
-	        //a class name to show what action now the row is under 
+	let pluginName = "multilevelStructure";
+	
+	let  defaults = {
+	        actionEl: "button",
 	        activeClass: "active",
-	        //a class name to show which row now is on focus
-	        //activeRow: "table-active",
-	        onSave: function(elem){ return true},
-	        fields: ["title","cost","period"], 
+	        prefixID: "lev_",
+	        //make a protection from adding and deleting necessary object property
+	        //incorporate the publish and subscriber methods separately from this module by the
+	        //event listeners and backward function with another publisher and subscribers updating page functionality
+	        fields: ["title","period","cost"],
+	        fldsSett: {
+	         title: {
+	           type: "input"
+	         },
+	         period: {
+             type: "input"
+           },
+           period_scale: {
+             type: "select",
+             list:   ['hours', 'days', 'weeks', 'months']
+           },
+           cost: {
+             type: "input",
+             format: 'dd.dd $'
+           },
+           done: {
+             type: "checkbox"
+           },
+	        } 
 		};
+
 	
     /* necessary html tags for working plugin properly */
     const structure = {
@@ -27,19 +46,26 @@
     };
     
     const readOnlyAttr="readonly";
-    const buttonNew="new";
-    const buttonModify="modify";
-    const buttonDiscard='discard';
-    const buttonSave='save';
-    const buttonMoveUp='moveUp';
-    const buttonMoveDown='doveDown';
-    const buttonEncreaseLevel='encreaseLevel';
-    const buttonDecreaseLevel='decreaseLevel';
-    const buttonsList=["edit","new","delete","save","moveUp","moveDown","levelDown","levelUp"];
-    const levelSeparator = "_";
-    const levelPrexif = "lev_";
+    const actions = ["create", "modify", 
+      "discard", "save", 
+      "moveUp", "moveDown", 
+      "encreaseLevel", 'decreaseLevel'];
+    
+    const btnsList = { 
+      create:  'Create new entity',
+      modify:  'Edit entity',
+      save: 'Save entity',
+      discard: 'Delete entity',
+      move_up: 'Move entity up',
+      move_down: 'Move entity down',
+      increase_level: 'Increase level of entity',
+      decrease_level: 'Increase level of entity'
+    };
+
+    const levelSep = "_";
     const dot = ".";
-    const idAttribute =  "id";    
+    const idAttr =  "id";   
+    const listFields = ["level","title","period","period_scale","cost","startFrom","endBy"];
     
     
 	
@@ -50,44 +76,46 @@
 		this._defauls = defaults;
 		this._name = pluginName;
     this.init();
+    
+    
 	}	
 	
+	
+	/*
+	 * initializing action for each button of the table row
+	 */
 	Plugin.prototype.init = function ()
 	{
 		let self = this;	   
-		$(this.element).click(updateActions);
-
-	}
-	
-	const updateActions = function(){
+		$(this.element).click(function() {
 	  
-	  if($(this).hasClass('new') === true)
-	      addDeliverable($(this));
+	    if($(this).hasClass('new') === true)
+	      self.addDeliverable($(this));
 	  
-	  if($(this).hasClass('level_down') === true)
+	     if($(this).hasClass('decrLevel') === true)
 	    decreaseLevel($(this));
-	  
+		});
+		
 	}
 	
 	
-    //new row is added into the table
-	  const addDeliverable = function(btn)
+    /*
+     * creating new row with all separate fields, buttons and ordial level number
+     */
+	  Plugin.prototype.addDeliverable = function(btn)
     {
 	    
 	    let settings = $(btn).data('plugin_' + pluginName).settings;
 		  let current_row=$(btn).closest('tr');
 		  
-		  
-		  letNewID = increaseOrderNumber($(current_row).attr(idAttribute));
-		  
-		  
-		  let btns =  $(deliverableTemplate())
-		    .attr(idAttribute,letNewID)
+		  //generating new level ID accroding to current tabel row
+		  levNewID = incOrdinalNumber($(current_row).attr(idAttr));
+		
+		  let btns =  $(entityTemplate(settings))
+		    .attr(idAttr,levNewID)
 		    .insertAfter(current_row)
 		    .find('button');	  
 		  
-		  
-		  //try to use function name instead
 		  $(btns).each(function(){
 		    
 		    if ( !$.data(this, "plugin_" + pluginName )) {
@@ -95,11 +123,8 @@
 	        new Plugin(this, settings ));
 		    }
 		  });
-	
-		  
-		  
-		   
-		   let rowNew = $('#'+letNewID);
+			   
+		   let rowNew = $('#'+levNewID);
 
           stateEdit(rowNew,settings);
           
@@ -108,7 +133,7 @@
          
           while($(nextRow).index()>0)
           {
-            $(nextRow).attr(idAttribute,increaseOrderNumber($(nextRow).attr(idAttribute)));
+            $(nextRow).attr(idAttr,incOrdinalNumber($(nextRow).attr(idAttr)));
             nextRow = $(nextRow).next();
           }
 
@@ -116,62 +141,102 @@
 	
 	
 	/*
-	 * Template for WBS deliverable
+	 * Template for separate table row
 	 */
-	const deliverableTemplate = function()
-	{
-	  return '<tr>'+
-      '<th class="actions">'+
-      '<button class="new" name="create" type="button" form="delivery"><img src="img/new.png" title="Create" width="27" height="27" /></button>'+
-      '<button class="edit"  type="button" name="eidt" form="delivery"><img src="img/Edit_start.png" title="Edit" width="27" height="27" /></button>'+
-      '<button class="move_up" name="save" type="button" form="delivery"><img src="img/edit_end.png" title="Save" width="27" height="27" /></button>'+
-      '<button class="level_down" name="descend" type="button" form="delivery"><img src="img/level_down.png" title="Lower" width="27" height="27" /></button>'+
-      '<button class="level_up" name="ascend" type="button" form="delivery"><img src="img/level_up.png" width="27" title="Ascend" height="27" /></button>'+
-      '<button class="move_down" name="lower" type="button" form="delivery"><img src="img/move_down.png" width="27" title="Lower" height="27" />'+
-      '<button class="move_up" name="lift" type="button" form="delivery"><img src="img/move_up.png" width="27" height="27" /></button>'+
-      '<button class="tree_but btn btn-secondary" type="button" form="delivery"><img src="img/tree_open.png" width="27" height="27" /></button>'+  
-      '</th>'+
-   '<td class="level">1</td>'+
-   '<td class="title">'+
-    '<input type="text" class="title form-control" readonly="readonly" form="deliverable" title="Title" aria-label="Title"/>'+
-  '</td>'+
-  '<td class="time">'+
-  '<input type="text" value="" readonly="readonly" class="period form-control" form="deliverable" readonly="readonly" />     '+
-  '</td>'+
-  '<td class="scale_time">'+
-  '<select class="form-control">'+
-   '<option>hours</option>'+
-    '<option>days</option>'+
-    '<option>weeks</option>'+
-    '<option>months</option>'+
-    '</select>'+
-   '</td>'+
-   '<td class="cost">'+
-    '<input class="span2 cost form-control" type="text" readonly="readonly" value="0">'+
-  '</td>'+
-  '<td class="thousand">'+
-  '<input type="checkbox" name="thousand_1" class="thousand form-check-input" readonly="readonly"/>'+
-  '</td>'+
-  '<th class="actions">'+
-    '<button class="move_down btn btn-outline-secondary btn-sm" type="button" form="delivery">'+
-    '<img src="img/delete.png" width="27" height="27" /></button> '+
-   '</th>'
-   '</tr>';
+	const entityTemplate = function(settings) {
+	  
+	  let entity = document.createElement('tr');
+	  let entityDelete = null;
+	  
+	  //the panel with buttons
+	  let entityActions = document.createElement('th');
+	  entityActions.className = 'actions';
+	  
+	  //inserting each button to the button panel
+    for (let [action, title] of Object.entries(btnsList)) {
+       
+       if (action.toString() === 'discard') {
+         //separate button for deleting entry
+         entityDelete = document.createElement('th');
+         entityDelete.className = 'actions';
+         entityDelete.appendChild(addBtn(settings.actionEl, action, title));
+       }
+       else
+         entityActions.appendChild(addBtn(settings.actionEl, action, title));
+     }
+    
+    entity.appendChild(entityActions);
+	  
+	  //the column containing the level ID of the entity
+	  let entityFld = document.createElement('td');
+	  entityFld.className = 'level';
+	  entity.appendChild(entityFld);
+	  
+	  settings.fields.forEach(function(element) {
+	    
+	    entityFld = document.createElement('td');
+	    entityFld.className = element;
+	    entity.appendChild(entityFld);
+	    
+	  });
+
+	  if(entityDelete !== null)
+	    entity.appendChild(entityDelete);
+	  
+	  return entity;
+    
+	};
+	
+	
+	/*
+	 * draw button html and its necessary attributes
+	 */
+	const addBtn = function(el, action, title) {
+	  
+    let btn = document.createElement(el);
+    btn.className = action;
+    
+    if (el === 'button')
+    {
+      btn.setAttribute('name', action);
+      btn.setAttribute('value', action);
+      btn.setAttribute('title', title);
+      //use `${key} ${value}` to substitute classes by template
+    }
+    btn.innnerHTML = '<span class="invisible">' + title + '</span>';
+    
+    return btn;
 	}
 	
-
 	
+	/*
+	 * insert the input field into the entity of the multilevel table
+	 */
+  const addField = function() {
+    
+    let inp = document.createElement('input');
+    inp.setAttribute('type','text');
+    inp.setAttribute('readonly','readonly');
+    
+    //set title and aria-label from customized settings
+    if(this.settings.actionForm)
+      inp.setAttribute('form',this.settings.actionForm);
+    
+    //aditional class for users can be inserted
+    inp.className = element;
+  }
+	
+  
 	  /*
      * Changing level of the entry to make as a subdirectory of the parent entry
      */
-     const decreaseLevel = function(e)
-     {
+    const decreaseLevel = function(e) {
        
        let actionBtn = e || this;
          
        let currentRow = $(actionBtn).closest('tr');
 
-       let currentId = $(actionBtn).closest('tr').attr(idAttribute);
+       let currentId = $(actionBtn).closest('tr').attr(idAttr);
        let currentLevel = getLevel(currentId);
        let currentOrder = findOrderNumber(currentId);
        
@@ -193,26 +258,23 @@
        while (prevRowLevel === null  &&  i < rowSearch.length+1)
        {
          
-         if (similarLevel.test($(rowSearch[i]).attr(idAttribute))) 
-             prevRowLevel = $(rowSearch[i]).attr(idAttribute);
+         if (similarLevel.test($(rowSearch[i]).attr(idAttr))) 
+             prevRowLevel = $(rowSearch[i]).attr(idAttr);
          i++;
-       }
-       
+       }  
        
        if(prevRowLevel)
-         lastChildLevel = hasSubDeliv(prevRowLevel);
-       
-       
+         lastChildLevel = hasSubDeliv(prevRowLevel);       
        
        //let newLevelId = 0;
        if(lastChildLevel)
        {
          lastChildLevel++;
-         $(currentRow).attr(idAttribute,levelPrexif + previousNumber + levelSeparator + lastChildLevel);
+         $(currentRow).attr(idAttr,levelPrexif + previousNumber + levelSep + lastChildLevel);
        }
        else
        {          
-         $(currentRow).attr(idAttribute, levelPrexif + previousNumber + levelSeparator + 1);
+         $(currentRow).attr(idAttr, levelPrexif + previousNumber + levelSep + 1);
          //let subLevel 
          //if(!subLevel)
          //  return;
@@ -223,12 +285,11 @@
        
        while($(nextRow).index()>0)
        {
-         $(nextRow).attr(idAttribute,decreaseOrderNumber($(nextRow).attr(idAttribute)));
+         $(nextRow).attr(idAttr,decreaseOrderNumber($(nextRow).attr(idAttr)));
          nextRow = $(nextRow).next();
        }
 
      }
-	
 	  
      
      /*
@@ -270,8 +331,8 @@
          let numberLevel = currentId.slice(levelPrexif.length);
          
          let level = 0;
-         if(numberLevel.indexOf(levelSeparator) > -1)
-           level = numberLevel.split(levelSeparator);    
+         if(numberLevel.indexOf(levelSep) > -1)
+           level = numberLevel.split(levelSep);    
          
          
          return level+1;
@@ -283,12 +344,12 @@
         */
         const hasSubDeliv = function(currentId)
         {
-          let similarLevel = new RegExp('^' + currentId + levelSeparator + "\\d$");
+          let similarLevel = new RegExp('^' + currentId + levelSep + "\\d$");
           let lastChildLevel = null;     
           
           lastChildLevel =  $('#'+currentId).closest('tbody').find('tr').filter(function(index,element)
           { 
-             if(similarLevel.test($(this).attr(idAttribute))) 
+             if(similarLevel.test($(this).attr(idAttr))) 
                  return 1;
           }).length;
           
@@ -299,10 +360,10 @@
         /*
          * Find order number
          */
-        const findOrderNumber = function(currentId)
+       const findOrderNumber = function(currentId)
         {
           let parsedLevel = 0, 
-          findLast = currentId.lastIndexOf(levelSeparator);
+          findLast = currentId.lastIndexOf(levelSep);
 
           //to check all number for separator symbols must be determined to exclude them from digitals
           if(findLast>-1)
@@ -313,10 +374,10 @@
        /*
         * Increase order number of the same level
         */
-       const increaseOrderNumber = function(currentId)
+       const incOrdinalNumber = function(currentId)
        {
          let parsedLevel = 0, 
-         findLast = currentId.lastIndexOf(levelSeparator);
+         findLast = currentId.lastIndexOf(levelSep);
 
          //to check all number for separator symbols must be determined to exclude them from digitals
          if(findLast>-1)
@@ -341,10 +402,10 @@
        /*
         * Increase order number of the same level
         */
-       const decreaseOrderNumber = function(currentId)
+       const decOrdialNumber = function(currentId)
        {
          let parsedLevel = 0, 
-         findLast = currentId.lastIndexOf(levelSeparator);
+         findLast = currentId.lastIndexOf(levelSep);
 
          //to check all number for separator symbols must be determined to exclude them from digitals
          if(findLast>-1)
@@ -363,8 +424,7 @@
          }
          
          return false;
-       }
-        
+       }        
       
       
       /*
@@ -422,4 +482,4 @@
     
       
 
-})(jQuery);
+})(jQuery,window, document);
