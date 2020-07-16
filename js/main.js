fdfe1a0fa068dee9a121e6466a680297c26c7c03
Olga Zhilkova <mindfulbayhorse@gmail.com>
2020-07-16 14:08:49 +0300

    File structure for package with npm

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@
  */
 import ko from "knockout";
 import ViewWBS from './viewmodels/wbs.js';
+import '../less/theme.less';
+import '../css/wbs.css';
+import '../css/normalize.css';
 
 let viewWBS = new ViewWBS([]);
 ko.applyBindings(viewWBS);
